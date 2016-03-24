e19d69dc5bfcfd7f1d3e3d64d849f010a0187322
David <djsaun@gmail.com>
2016-03-24 18:46:27 -0500

    Added new parent themetheme; Updated functions.php

--- wp-content/themes/saunders-blog/functions.php
+++ wp-content/themes/saunders-blog/functions.php
@@ -2,6 +2,6 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
+    wp_enqueue_style( 'site-style', '/wp-content/themes/saunders-blog/css/app.min.css' );
 }
 ?>
