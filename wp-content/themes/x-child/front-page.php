76d77d63f2c6e5de5c4f276ef934c16073300033
David <djsaun+github@gmail.com>
2016-04-17 20:47:33 -0500

    Added rss logic to new file; Refined featured industry logic

--- wp-content/themes/x-child/front-page.php
+++ wp-content/themes/x-child/front-page.php
@@ -7,6 +7,7 @@
 // =============================================================================
 
 require_once('php/autoloader.php');
+
 ?>
 
 <?php get_header(); ?>
@@ -46,31 +47,92 @@ require_once('php/autoloader.php');
 
       <section class="featured-category">
         <?php
-        $dayOfWeek = jddayofweek ( cal_to_jd(CAL_GREGORIAN, date("m"),date("d"), date("Y")) , 1 );
+        date_default_timezone_set('America/Los_Angeles');
+
+        $timezone = date_default_timezone_get();
+        $dayOfWeek = date("l");
         $dayOfWeek = strtolower($dayOfWeek);
-        echo get_post_meta( $post->ID, 'rss_feed_input', true );
+        $currentDate = date("Y-m-d");
+
+
+        // var_dump($date);
+        //
+        // if (get_post_meta( $post->ID, 'priority_date', true ) == $currentDate) {
+        //   echo 'current date';
+        // } else {
+        //   echo 'not current date';
+        // }
         $featuredCategoryQuery = new WP_Query(
            array(
              'post_type' => 'x-portfolio',
              'order_by' => 'menu-order',
              'order' => 'ASC',
-             'posts_per_page' => 1,
-             'meta_query' => array(
-                array(
-                   'key' => $dayOfWeek,
-                   'value' => 'checked',
-                   'compare' => '='
-                )
-             )
+             'posts_per_page' => 1
            )
          );
+
+        // $featuredCategoryQuery = new WP_Query(
+        //    array(
+        //      'post_type' => 'x-portfolio',
+        //      'order_by' => 'menu-order',
+        //      'order' => 'ASC',
+        //      'posts_per_page' => 1,
+        //      'meta_query' => array(
+        //         array(
+        //            'key' => $dayOfWeek,
+        //            'value' => 'checked',
+        //            'compare' => '='
+        //         )
+        //      )
+        //    )
+        //  );
 
          if ( $featuredCategoryQuery->have_posts() ) {
 
           // Start looping over the query results.
           while ( $featuredCategoryQuery->have_posts() ) {
 
-          $featuredCategoryQuery->the_post(); ?>
+          $featuredCategoryQuery->the_post();
+
+          $dateQuery = new WP_Query(
+             array(
+               'post_type' => 'x-portfolio',
+               'posts_per_page' => 1,
+               'meta_query' => array(
+                 'relation' => 'AND',
+                  array(
+                    'key' => 'priority_date',
+                    'compare' => 'EXISTS'
+                  ),
+                  array(
+                    'key' => 'priority_date',
+                    'value' => $currentDate,
+                    'compare' => '='
+                  )
+                )
+             )
+           );
+
+           $dayQuery = new WP_Query(
+              array(
+                'post_type' => 'x-portfolio',
+                'posts_per_page' => 1,
+                'meta_query' => array(
+                   array(
+                     'key' => $dayOfWeek,
+                     'value' => 'checked',
+                     'compare' => '='
+                   )
+                 )
+              )
+            );
+
+           if ( $dateQuery->have_posts() ) {
+
+            // Start looping over the query results.
+            while ( $dateQuery->have_posts() ) {
+
+            $dateQuery->the_post(); ?>
 
             <h3 class="featured-category-title"><?php echo the_title(); ?> News</h3>
 
@@ -84,58 +146,39 @@ require_once('php/autoloader.php');
               </div>
 
               <div class="x-column x-sm x-1-3 categories">
-                <?php
-                $args = array( 'post_type' => 'rss_feed' );
-
-                $loop = new WP_Query( $args );
-                while ( $loop->have_posts() ) : $loop->the_post();
-                  $rss = get_post_meta( get_the_ID(), 'rss_feed_input', true );
-                  $feed = new SimplePie();
-
-                  // Set which feed to process.
-                  $feed->set_feed_url(explode(',',$rss));
-                  $feed->enable_cache(true);
-                  $feed->set_stupidly_fast(true);
-                  $feed->enable_order_by_date(true);
-                  // Run SimplePie.
-                  $feed->init();
-
-                  // This makes sure that the content is sent to the browser as text/html and the UTF-8 character set (since we didn't change it).
-                  $feed->handle_content_type();
-
-                  foreach ($feed->get_items(0, 5) as $item):
-                  ?>
-
-                    <div class="item">
-                      <h2><a href="<?php echo $item->get_permalink(); ?>"><?php echo $item->get_title(); ?></a></h2>
-                      <p><?php echo $item->get_description(); ?></p>
-                      <p><small>Posted on <?php echo $item->get_date('j F Y | g:i a'); ?></small></p>
-                    </div>
-
-                  <?php endforeach;
-
-                endwhile;
-                 ?>
-
-                <?php
-                /*
-                Here, we'll loop through all of the items in the feed, and $item represents the current item in the loop.
-                */
-                foreach ($feed->get_items() as $item):
-                ?>
-
-                  <div class="item">
-                    <h2><a href="<?php echo $item->get_permalink(); ?>"><?php echo $item->get_title(); ?></a></h2>
-                    <p><?php echo $item->get_description(); ?></p>
-                    <p><small>Posted on <?php echo $item->get_date('j F Y | g:i a'); ?></small></p>
-                  </div>
-
-                <?php endforeach; ?>
+                <?php include("rss.php"); ?>
               </div>
 
         <?php  }
 
-        } ?>
+      }
+      else {
+        if ( $dayQuery->have_posts() ) {
+
+         // Start looping over the query results.
+         while ( $dayQuery->have_posts() ) {
+
+         $dayQuery->the_post(); ?>
+
+         <h3 class="featured-category-title"><?php echo the_title(); ?> News</h3>
+
+           <div class="x-column x-sm x-1-3 categories">
+             <!-- <iframe style=”border: 1px solid #333333; overflow: hidden; width: 190px; height: 490px;” src=”https://research.stlouisfed.org/fred-glance-widget.php” height=”240″ width=”320″ frameborder”0″ scrolling=”no”></iframe> -->
+             <iframe src="<?php echo get_post_meta( $post->ID, 'portfolio_widget', true ); ?>"></iframe>
+           </div>
+
+           <div class="x-column x-sm x-1-3 categories">
+               test
+           </div>
+
+           <div class="x-column x-sm x-1-3 categories">
+             <?php include("rss.php"); ?>
+           </div>
+        <?php  }
+       }
+      }
+    }
+  }?>
       </section>
     </div>
 
